6c940e316186f810a759f785a027858bdef9bf89
Alexei Andreev <leshakod@gmail.com>
2024-01-18 11:56:38 +0300

    Set .env default values of configs

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_DATABASE'],
-    'username' => $_ENV['DB_USER'],
-    'password' => $_ENV['DB_PASSWORD'],
-    'charset' => $_ENV['DB_CHARSET'],
+    'dsn' => 'mysql:host=' . ($_ENV['DB_HOST'] ?? 'mysql_db') . ';dbname=' . ($_ENV['DB_DATABASE'] ?? 'stage'),
+    'username' => $_ENV['DB_USER'] ?? 'root',
+    'password' => $_ENV['DB_PASSWORD'] ?? 'secret',
+    'charset' => $_ENV['DB_CHARSET'] ?? 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
