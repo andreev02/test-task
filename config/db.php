fe97738e9513a68f860b034a0a9fda8c2ead9dda
Alexei Andreev <leshakod@gmail.com>
2024-01-18 11:13:19 +0300

    Added .env

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql_db;dbname=stage',
-    'username' => 'root',
-    'password' => 'secret',
-    'charset' => 'utf8',
+    'dsn' => 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'charset' => $_ENV['DB_CHARSET'],
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
